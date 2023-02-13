89bb978ce072cdb970fa9d2caa219d87fa2a037a
Shreejala Tuladhar <shreejala.tuladhar@gurzu.com>
2023-02-13 13:04:04 +0545

    feat: Fix onclear image on unmount

--- src/ReceiveSharingIntent.ts
+++ src/ReceiveSharingIntent.ts
@@ -1,6 +1,6 @@
 import type { IReceiveSharingIntent, IUtils } from "./ReceiveSharingIntent.interfaces";
 import { Platform, Linking, AppState, NativeModules } from "react-native";
-import  Utils from "./utils";
+import Utils from "./utils";
 
 const { ReceiveSharingIntent } = NativeModules;
 
@@ -9,49 +9,51 @@ class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     private utils: IUtils = new Utils();
     private isClear: boolean = false;
 
-    getReceivedFiles(handler: Function, errorHandler: Function, protocol: string = "ShareMedia"){
-        if(this.isIos){
-            Linking.getInitialURL().then((res:any) => {
+    getReceivedFiles(handler: Function, errorHandler: Function, protocol: string = "ShareMedia") {
+        if (this.isIos) {
+            Linking.getInitialURL().then((res: any) => {
                 if (res && res.startsWith(`${protocol}://dataUrl`) && !this.isClear) {
                     this.getFileNames(handler, errorHandler, res);
                 }
             }).catch(() => { });
-            Linking.addEventListener("url", (res:any) => {
+            Linking.addEventListener("url", (res: any) => {
                 const url = res ? res.url : "";
                 if (url.startsWith(`${protocol}://dataUrl`) && !this.isClear) {
-                    this.getFileNames(handler,errorHandler, res.url);
+                    this.getFileNames(handler, errorHandler, res.url);
                 }
             });
-        }else{
-            AppState.addEventListener('change', (status: string) => {
-                if (status === 'active' && !this.isClear) {
-                    this.getFileNames(handler,errorHandler, "");
-                }
-              });
-           if(!this.isClear) this.getFileNames(handler,errorHandler, "");
+        } else {
+            this.getFileNames(handler, errorHandler, "");
+            //     AppState.addEventListener('change', (status: string) => {
+            //         if (status === 'active' && !this.isClear) {
+            //             this.getFileNames(handler,errorHandler, "");
+            //         }
+            //       });
+            //    if(!this.isClear) this.getFileNames(handler,errorHandler, "");
         }
     }
 
-    clearReceivedFiles(){
-        this.isClear = true;
+    clearReceivedFiles() {
+        // this.isClear = true;
+        ReceiveSharingIntent.clearFileNames();
     }
 
-    
-   protected getFileNames(handler: Function, errorHandler: Function, url: string){
-        if(this.isIos){
-            ReceiveSharingIntent.getFileNames(url).then((data: any)=>{         
-                 let files = this.utils.sortData(data);
-                 handler(files);
-            }).catch((e:any)=>errorHandler(e));
-        }else{
+
+    protected getFileNames(handler: Function, errorHandler: Function, url: string) {
+        if (this.isIos) {
+            ReceiveSharingIntent.getFileNames(url).then((data: any) => {
+                let files = this.utils.sortData(data);
+                handler(files);
+            }).catch((e: any) => errorHandler(e));
+        } else {
             ReceiveSharingIntent.getFileNames().then((fileObject: any) => {
                 let files = Object.keys(fileObject).map((k) => fileObject[k])
                 handler(files);
-            }).catch((e:any)=>errorHandler(e));
+            }).catch((e: any) => errorHandler(e));
         }
     }
 
-    
+
 }
 
 export default ReceiveSharingIntentModule;
